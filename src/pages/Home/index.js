b7c1fa82fa09c1929b09711fac6b03f807b5f689
ViniciusItakura <viniciusitakura1@gmail.com>
2023-12-06 16:52:18 -0300

    Criado integração com a API para puxar todos os filmes e colocar estilizado na Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api.js';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 //URL API movie/now_playing?api_key=b3ce7e92a7805422db24cdbaf3bbe122&language=pt-BR
 
@@ -16,15 +18,26 @@ function Home() {
                 }
             });
 
-            console.log(response);
+            //console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10));
         }
 
         loadFilmes();
     }, []);
 
     return (
-        <div>
-            <h1>Bem vindo a home</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}></img>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    );
+                })}
+            </div>
         </div>
     );
 }
